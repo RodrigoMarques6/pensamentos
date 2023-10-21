5318217652fe3ec0efea87cd505f26d1fe69f6a9
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2023-10-20 21:55:41 -0300

    Ajustes no componente pensamento

--- src/app/components/pensamentos/pensamento/pensamento.component.ts
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -6,14 +6,20 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./pensamento.component.css'],
 })
 export class PensamentoComponent implements OnInit {
-
   @Input() pensamento = {
-      conteudo: 'Eu amo Angular',
-      autoria: 'Rodrigo Marques',
-      modelo: 'modelo3',
-  }
+    conteudo: 'Eu amo Angular',
+    autoria: 'Rodrigo Marques',
+    modelo: 'modelo3',
+  };
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
+    }
+    return 'pensamento-p';
+  }
 }
