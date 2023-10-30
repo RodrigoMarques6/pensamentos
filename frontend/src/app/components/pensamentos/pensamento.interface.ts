32e37c4ed98076f24f4e7b25f8f95370a87bd3c6
rodrigomarques6 <rodrigomarques328@hotmail.com>
2023-10-30 09:47:50 -0300

    Ajustando módulos da aplicação e adicionando o arquivo service

--- frontend/src/app/components/pensamentos/pensamento.interface.ts
+++ frontend/src/app/components/pensamentos/pensamento.interface.ts
@@ -4,3 +4,6 @@ export interface Pensamento {
   autoria: string,
   modelo: string,
 }
+
+// Interfaces tem o objeto de padronizar tanto o código, quanto os dados advindos do front end para serem armazenados no banco.
+// Lembrar que é como se fosse um contrato e precisa ser respeitado tando pelo front quanto pelo back.
---
+++ frontend/src/app/components/pensamentos/pensamento.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+  // ProvidedIn significa "provedor", isto é, ele pode ser utilizado em todas as partes da aplicação.
+})
+export class PensamentoService {
+  constructor() {}
+}
