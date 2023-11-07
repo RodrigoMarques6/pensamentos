1784309a13f3e988dcaa79ae1c801050fe27265a
rodrigomarques6 <rodrigomarques328@hotmail.com>
2023-11-07 10:59:10 -0300

    Adicionando validações nos inputs do form

--- frontend/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ frontend/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -24,13 +24,23 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['', [Validators.required]],
-      autoria: [''],
+      conteudo: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        ]),
+      ],
+      autoria: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
       modelo: ['Modelo2'],
     });
   }
 
   criarPensamento() {
+    // Parte da validação do formulário de acordo com os Validators:
     if (this.formulario.valid) {
       this.service.criar(this.formulario.value).subscribe();
       // Relembrando que o subscribe é um método que fica aguardando uma resposta para avisar assim que obver
